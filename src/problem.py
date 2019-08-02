6aa1f1fae876be62addb5b9f813c137c59ef4b8e
Frank <frankli0324@hotmail.com>
2019-08-02 14:19:22 +0800

    add update function

--- src/problem.py
+++ src/problem.py
@@ -21,6 +21,19 @@ class Problems(db.Model):
         self.cases = json.dumps(cases)
 
 
+def check_limits_fmt(l):
+    for i in ['max_cpu_time', 'max_memory', 'max_output_size',
+              'max_real_time', 'max_stack', 'max_process_number']:
+        if i not in l.keys():
+            raise Exception()
+
+
+def check_cases_fmt(c):
+    for i in c:
+        if type(i['input']) != str or type(i['output']) != str:
+            raise Exception()
+
+
 @problem.route('/add', methods=['POST'])
 @utils.api_call
 def add_problem():
@@ -31,13 +44,8 @@ def add_problem():
                 raise Exception()
         if type(r['cases']) != list:
             raise Exception()
-        for i in ['max_cpu_time', 'max_memory', 'max_output_size',
-                  'max_real_time', 'max_stack', 'max_process_number']:
-            if i not in r['limits'].keys():
-                raise Exception()
-        for i in r['cases']:
-            if type(i['input']) != str or type(i['output']) != str:
-                raise Exception()
+        check_limits_fmt(r['limits'])
+        check_cases_fmt(r['cases'])
     except Exception:
         return 403, 'wrong parameters', None
 
@@ -51,6 +59,33 @@ def add_problem():
     return 200, 'success', {
         'problem_id': prob.id
     }
+
+
+@problem.route('/update/<int:prob_id>/<column>', methods=['POST'])
+@utils.api_call
+def update(prob_id, column):
+    if column not in ['cases', 'limits']:
+        return 404, 'no such column', None
+    r = request.json
+    prob = Problems.query.filter_by(id=prob_id).first()
+    if not prob:
+        return 404, 'no such problem'
+    if column == 'cases':
+        try:
+            check_cases_fmt(r)
+        except Exception:
+            return 403, 'wrong parameters', None
+        prob.cases = r
+        prob.case_cnt = len(r)
+    else:
+        try:
+            check_limits_fmt(r)
+        except Exception:
+            return 403, 'wrong parameters', None
+        prob.limits = r
+    db.session.add(prob)
+    db.session.commit()
+    return 200, 'success', None
 
 
 @problem.route('/info/<int:prob_id>')
